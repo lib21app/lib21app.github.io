5d108764a1f106e184c8897115cbeb8b8b0ec346
@faridiot <defaultride.bruno@gmail.com>
2022-08-17 10:54:57 +0700

    media pembelajaran

--- js/rekomendasi.js
+++ js/rekomendasi.js
@@ -97,3 +97,13 @@ $(elems[i]).hide();
 }
 }
 }
+
+var elems = $("mediapembelajaran");
+if (elems.length) {
+var keep = Math.floor(Math.random() * elems.length);
+for (var i = 0; i < elems.length; ++i) {
+if (i !== keep) {
+$(elems[i]).hide();
+}
+}
+}
