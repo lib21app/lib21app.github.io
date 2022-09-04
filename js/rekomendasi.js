9f96d8107d941955e9e04a643d9d9b00d40a8c59
@faridiot <defaultride.bruno@gmail.com>
2022-09-04 20:05:24 +0700

    ok

--- js/rekomendasi.js
+++ js/rekomendasi.js
@@ -187,3 +187,13 @@ $(elems[i]).hide();
 }
 }
 }
+
+var elems = $("sketsa");
+if (elems.length) {
+var keep = Math.floor(Math.random() * elems.length);
+for (var i = 0; i < elems.length; ++i) {
+if (i !== keep) {
+$(elems[i]).hide();
+}
+}
+}
