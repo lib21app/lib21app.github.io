bb853da801fc7e19862a2f340fa14db7c139f8fa
@faridiot <defaultride.bruno@gmail.com>
2022-10-11 12:29:43 +0700

    ok

--- js/jambuka.js
+++ js/jambuka.js
@@ -1,11 +1,11 @@
 			$(function () {
 				$('#ops-container').openStatus({
 					sunday : 'closed until Monday',
-					monday : 'open from 7am until 12.00pm',
-					tuesday : 'open from 7am until 14.00pm',
-					wednesday : 'open from 7am until 14.00pm',
-					thursday : 'open from 7am until 14.00pm',
-					friday : 'open from 7am until 11.00pm',
-					saturday : 'open from 7am until 12.00pm'
+					monday : 'open from 7am until 12.00pm (view more)',
+					tuesday : 'open from 7am until 14.00pm (view more)',
+					wednesday : 'open from 7am until 14.00pm (view more)',
+					thursday : 'open from 7am until 14.00pm (view more)',
+					friday : 'open from 7am until 11.00pm (view more)',
+					saturday : 'open from 7am until 12.00pm (view more)'
 				});
 			});
